ea82299fa9d5b9307bb4173e910f83206c2942fa
Sean Leach <13541397+srleach@users.noreply.github.com>
2025-05-18 14:47:20 +0100

    Read config as UTF-8 for proper ingestion of degrees symbol

--- spa2mqtt/utils/spa_configuration.py
+++ spa2mqtt/utils/spa_configuration.py
@@ -2,12 +2,12 @@ import yaml
 
 
 def get_application_configuration(file: str = 'config.yml'):
-    with open(file, 'r') as f:
+    with open(file, 'r', encoding="utf-8") as f:
         return yaml.safe_load(f)
     #TODO handle failure
 
 
 def get_variant_configuration(file: str = 'base'):
-    with open(f"spa2mqtt/spa_definitions/{file}.yml", 'r') as f:
+    with open(f"spa2mqtt/spa_definitions/{file}.yml", 'r', encoding="utf-8") as f:
         return yaml.safe_load(f)
     #TODO handle failure

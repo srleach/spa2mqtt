958a444a3b48bc619a9c118664536a2247b31928
Sean Leach <13541397+srleach@users.noreply.github.com>
2025-05-20 14:49:45 +0100

    Pull from env - we should improve this approach

--- spa2mqtt/utils/spa_configuration.py
+++ spa2mqtt/utils/spa_configuration.py
@@ -1,10 +1,25 @@
+import os
+
 import yaml
 
 
 def get_application_configuration(file: str = 'config.yml'):
-    with open(file, 'r', encoding="utf-8") as f:
-        return yaml.safe_load(f)
-    #TODO handle failure
+    try:
+        with open(file, 'r', encoding="utf-8") as f:
+            return yaml.safe_load(f)
+    except FileNotFoundError as err:
+        print("Warning: Config file not found. Using some defaults")
+
+        return {
+            "spa": {
+                "connection": {
+                    "spa_address": os.environ.get("SPA_IP", "192.168.1.1"),
+                    "spa_port": os.environ.get("SPA_PORT", "8899"),
+                },
+                "config": os.environ.get("VARIANT_DEFINITION", "config.yml"),
+                "model": "Default Model"
+            }
+        }
 
 
 def get_variant_configuration(file: str = 'base'):
